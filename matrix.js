565fbf7e2eacd0959b020f7be2ab66486155bb68
Wistful <wickedvirtue@yahoo.com>
2018-01-21 10:33:38 -0500

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -211,9 +211,9 @@ Matrix.prototype.visualize = function(idSelector) {
 }
 
 Matrix.prototype.pprint = function(){ //"pretty print" the matrix
-  let fstring = '['
+  let fstring = '[';
   for (let i=0;i<this.matrix.length;i++){
-    fstring +=  (i!=0?' ':'') + ` [${this.matrix[i].map(x=>' ' + x.toString() + ' ')}],\n`
+    fstring +=  (i!=0?' ':'') + ` [${this.matrix[i].map(x=>' ' + x.toString() + ' ')}],\n`;
   }
-  console.log(fstring.substring(0,fstring.length-2) + ' ]')
+  console.log(fstring.substring(0,fstring.length-2) + ' ]');
 }
